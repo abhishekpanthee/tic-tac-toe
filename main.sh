0d51106719846c22c102fd61d4bb67c8c8f59278
Abhishek Panthee <80570587+abhishekpanthee@users.noreply.github.com>
2024-05-15 08:36:21 +0545

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,59 +1,29 @@
-#! /bin/bash
-Black="\e[30m" ; White="\e[37m"
-Green="\e[32m" ; Red="\e[31m"
-Yellow="\e[33m"; Blue="\e[34m"
-Purple="\e[35m"; Aqua="\e[36m"
-Noc="\e[0m"
-
-
-
-pause_(){ read -t 2 -sn1 -p "Press any key to continue..."; }
-
-draw_screen_(){
-	clear
-
-	board=( " ${xy[0]} | ${xy[1]} | ${xy[2]}"\
-		"---|---|---"\
-		" ${xy[3]} | ${xy[4]} | ${xy[5]}"\
-		"---|---|---"\
-		" ${xy[6]} | ${xy[7]} | ${xy[8]}")
-	printf "\n"
-	for (( i=0; i<5; i++ )); do echo -e "    ${board[$i]}"; done
-	printf "\n\n"
-}
-
-who_wins_(){
-	case "$PlayingMode" in
-		1)
-			[[ "$Player1" == "X" ]] && ResultX="You win!" && ResultO="Better luck next time!"
-			[[ "$Player1" == "O" ]] && ResultO="You win!" && ResultX="Better luck next time!"
-		;;
-		2)
-			[[ "$Player1" == "X" ]] && ResultX="${PName1} Wins!" && ResultO="${PName2} Wins!"
-			[[ "$Player1" == "O" ]] && ResultX="${PName2} Wins!" && ResultO="${PName1} Wins!"
-		;;
-	esac
-	ResultD="Match draw!"
-
-	# Rules to win
-	if [[ "${xy[1]}${xy[0]}" == "${xy[0]}${xy[2]}" || "${xy[3]}${xy[0]}" == "${xy[0]}${xy[6]}" ]]; then
-		result="Result${xy[1]}";
-	elif [[ "${xy[3]}${xy[4]}" == "${xy[4]}${xy[5]}" || "${xy[1]}${xy[4]}" == "${xy[4]}${xy[7]}" ]]; then
-		result="Result${xy[4]}"
-	elif [[ "${xy[6]}${xy[8]}" == "${xy[8]}${xy[7]}" || "${xy[2]}${xy[8]}" == "${xy[8]}${xy[5]}" ]]; then
-		result="Result${xy[8]}"
-	elif [[ "${xy[0]}${xy[4]}" == "${xy[4]}${xy[8]}" || "${xy[2]}${xy[4]}" == "${xy[4]}${xy[6]}" ]]; then
-		result="Result${xy[4]}"
-	elif [[ "${xy[@]}" != *[1-9]* ]]; then
-		result="ResultD"
-	fi
-
-	if [[ ! -z "$result" ]]; then
-		clear; sleep 0.4
-		printf "${Green}${!result}$Noc\n\n"
-		unset result
-		pause_
-		main_ "@"
-	fi
-}
-#player needed
+#!/bin/bash
+command -v curl >/dev/null 2>&1 || exit 1
+green='\033[30;102m'; yellow='\033[30;103m'; white='\033[30;107m'; reset='\033[0m'
+wordle_dir="${XDG_DATA_HOME:-$HOME/.local/share}/wordle"
+mkdir -p "${wordle_dir}" || exit 1
+dict="https://gist.githubusercontent.com/prichey/95db6bdef37482ba3f6eb7b4dec99101/raw/"
+if [[ ! -f "${wordle_dir}/wordles" ]]; then
+  curl -s "${dict}" | tr '[:lower:]' '[:upper:]' > "${wordle_dir}/wordles"
+fi
+mapfile -t words < "${wordle_dir}/wordles" || exit 1
+actual=${words[$((RANDOM % ${#words[@]}))]}
+[[ $1 == "unlimit" ]] && max_guess=999999
+for (( guess_count=1; guess_count<=${max_guess:-6}; guess_count++ )); do
+    until [[ " ${words[*]} " =~ " ${guess^^} " ]]; do
+      printf -- '%s\n' "Please enter a valid 5 letter word..."
+      read -rp "Enter your guess (${guess_count} / ${max_guess:-6}): " guess
+    done
+    guess="${guess^^}"; output=""; remaining=""
+    if [[ "${actual}" == "${guess}" ]]; then
+        printf -- '%s\n' "You guessed right!"
+        for ((i=0; i<5; i++)); do
+            output+="${green} ${guess:$i:1} ${reset}"
+        done
+        printf -- '%b\n' "${output}"; exit 0
+    fi
+    for ((i=0; i<5; i++)); do
+        [[ "${actual:$i:1}" != "${guess:$i:1}" ]] && remaining+=${actual:$i:1}
+    done
+    
